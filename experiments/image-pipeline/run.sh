ea5c6f4275b7685f71c2d28fdc12fa1c10177dab
clstb <claas.stoertenbecker@gmail.com>
2022-07-16 17:20:04 +0200

    feat: add image-pipeline experiment

---
+++ experiments/image-pipeline/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+export HOST=$1
+NODES=$2
+
+for i in ${NODES//,/ }
+do
+  mkdir -p ./$i
+done
+
+vegeta attack -rate=1/s -lazy -format=json -duration=5m < <(
+    while true; do
+        export BODY=$(echo "-Ls https://picsum.photos/1000 --output -" | curl -s -XPOST --data-binary @- http://$HOST/function/curl --output -)
+        jq -ncM --arg HOST "$HOST" --arg BODY "$BODY" '{method: "POST", url: "http://\(env.HOST)/function/shasum", body: env.BODY | @base64, header: {"Content-Type": ["text/plain"]}}'
+    done
+) | \
+    tee latencies.bin | \
+    vegeta report -every=200ms &
+
+while [[ -n $(jobs -r) ]]; do
+  for i in ${NODES//,/ }
+  do
+    echo $[100-$(multipass exec $i -- vmstat 1 2|tail -1|awk '{print $15}')] >> ./$i/cpu-raw.txt;
+    multipass exec $i -- free | sed -n '2p' | awk '{print $3}' >> ./$i/ram-raw.txt;
+  done
+  sleep 1;
+done
+
+for i in ${NODES//,/ }
+do
+  awk '{printf "%s,%s\n",NR,$1}' ./$i/cpu-raw.txt > ./$i/cpu.csv
+  awk '{printf "%s,%s\n",NR,$1/1024}' ./$i/ram-raw.txt > ./$i/ram.csv
+done
+cat latencies.bin | vegeta report -type=hdrplot | awk 'NR>1 { printf "%s,%s\n", $2, $1}' > latencies.csv
